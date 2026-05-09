db1df830d31d0ec38d54798f6ad4928f633db273
Arjun Chandrasekaran <c14.arjun@gmail.com>
2026-05-09 11:55:48 +0530

    commiting project5.py

---
+++ project5.py
@@ -0,0 +1,6 @@
+temperature=int(input("enter the temperature: "))
+print(temperature)
+if temperature>15:
+    print("wear normal clothes, like a t-shirt")
+else:
+    print("wear warm clothes, like a hoodie")
