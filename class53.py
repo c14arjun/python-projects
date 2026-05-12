48f65d7bd5e3acc3358499ce27c639acf26f1ce5
Arjun Chandrasekaran <c14.arjun@gmail.com>
2026-05-12 10:10:49 +0530

    commiting class5 part3

---
+++ class53.py
@@ -0,0 +1,12 @@
+a=4
+b=4
+c=5
+if not ((a==1)==(b==4)):
+    print("hello")
+else:
+    print("not equal)")
+a=int(input("enter a number: "))
+if not (a%2==0):
+    print("it is an odd number")
+else:
+    print("even")
