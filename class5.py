f0d0d79e50dd4dde2113842a4e3a5c1ac02d1f29
Arjun Chandrasekaran <c14.arjun@gmail.com>
2026-05-12 09:47:23 +0530

    commited class5.py part 1

---
+++ class5.py
@@ -0,0 +1,15 @@
+a=25
+b=-30
+if a>0 and b>0:
+    print("both are positive")
+else:
+    print("both are not positive")
+
+a=25
+b=-30
+if a>0 or b>0:
+    print("both are positive")
+else:
+    print("both are not positive")
+
+
