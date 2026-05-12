59209783f4754fc6d16c4a5c792a86484cce27c3
Arjun Chandrasekaran <c14.arjun@gmail.com>
2026-05-12 09:59:42 +0530

    commiting part 2 of class 5

---
+++ class52.py
@@ -0,0 +1,21 @@
+
+a=10
+b=-10
+c=0
+if a>0 and b>0 and c>0:
+    print("All values have boolean value as true")
+else:
+    print("Atleast one value has boolean value as true")
+d=30
+e=-30
+f=25
+
+if d>e or d>f:
+    print("Either d is greater than e and f")
+else:
+    print("neither d nor e is greater")
+c=-30
+if not c>0:
+    print("True")
+else:
+    print("false")
