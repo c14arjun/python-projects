7c2021641e60a008b9a3a5e9021753bcbe239da5
Arjun Chandrasekaran <c14.arjun@gmail.com>
2026-05-19 10:10:48 +0530

    commiting class 7 part 3

---
+++ class73.py
@@ -0,0 +1,10 @@
+mean1=38
+wrong_number=36
+correct_number=56
+total_number=40
+result=mean1*total_number
+print(result)
+num2=result-(wrong_number-correct_number)
+print(num2)
+mean2=num2/total_number
+print(mean2)
