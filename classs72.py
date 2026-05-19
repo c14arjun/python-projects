e55d0c47c83d3f0422399dd031429720652bbe93
Arjun Chandrasekaran <c14.arjun@gmail.com>
2026-05-19 10:01:13 +0530

    commiting class 7 part 2.py

---
+++ classs72.py
@@ -0,0 +1,9 @@
+num1=float(input("enter your first number: "))
+num2=float(input("enter your second number: "))
+result=num1%num2
+print(result)
+if result==0:
+    print("Numbers are divisible")
+else:
+    print("not divisible")
+
