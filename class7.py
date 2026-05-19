d151b12ad725fb172f9cc94279fc4f64535738b0
Arjun Chandrasekaran <c14.arjun@gmail.com>
2026-05-19 09:52:44 +0530

    commiting class 7.py

---
+++ class7.py
@@ -0,0 +1,19 @@
+result=5+3+2
+print(result)
+b=9-(8+2)
+print(b)
+c=((6*9)+(3*2)/5)
+print(c)
+v=5
+w=2
+x=9
+y=6
+z=0
+z=(v+w)*x/y
+print(z)
+name="Jack"
+age=9
+if name=="Jack" or name == "Alex" and age >= 9:
+    print("Welcome")
+else:
+    print("Good bye")
---
+++ project6.py
@@ -0,0 +1,6 @@
+charecter = input("Enter a character: ")
+
+if ('a' <= charecter <= 'z'):
+    print("It is a letter")
+else:
+    print("It is not a letter")
