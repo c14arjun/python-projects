a7cec85adbad8acbdff8bf9f2948fa4b786f7844
Arjun Chandrasekaran <c14.arjun@gmail.com>
2026-06-09 17:15:28 +0530

    c

--- test1.py
+++ test1.py
@@ -1,4 +1,4 @@
-secret=27
+secret=13
 print("you have 5 attempts to guess the number")
 i=1
 while i<=5:
@@ -9,25 +9,25 @@ while i<=5:
         i=i+4
     elif i==2:
         print("wrong,❤️❤️❤️❤️ left")
-        if guess<27:
+        if guess<13:
             print("guess is too low")
         else:
             print("guess is too high")
     elif i==3:
         print("wrong,❤️❤️️❤️ left")
-        if guess<27:
+        if guess<13:
             print("guess is too low")
         else:
             print("guess is too high")
     elif i==4:
         print("wrong,❤️❤️ left")
-        if guess<27:
+        if guess<13:
             print("guess is too low")
         else:
             print("guess is too high")
     elif i==5:
         print("wrong,❤️ left")
-        if guess<27:
+        if guess<13:
             print("guess is too low")
         else:
             print("guess is too high")
