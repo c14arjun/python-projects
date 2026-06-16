6fccd56367faa4dfd681b5a635b559773c3bd867
Arjun Chandrasekaran <c14.arjun@gmail.com>
2026-06-16 16:41:32 +0530

    c

---
+++  class134.py
@@ -0,0 +1,7 @@
+def area(side):
+    return side*side
+def perimeter(side):
+    return side*4
+num=int(input("enter a number: "))
+print(f"area of square is {area(num)}")
+print(f"perimeter of square is {perimeter(num)}")
