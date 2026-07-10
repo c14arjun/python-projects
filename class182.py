afff50522a952d20cc08666df4859325e96c3aa5
Arjun Chandrasekaran <c14.arjun@gmail.com>
2026-07-10 16:26:49 +0530

    practice.py

---
+++ class182.py
@@ -0,0 +1,10 @@
+list=[]
+list1=[1,2,3,4,5,9,5,2,4,5,6]
+print(list1)
+list2=list1*2
+print(list2)
+reverse_list= list1[::-1]
+print(reverse_list)
+print(f"the minimum number in this list is {min(list1)}")
+print(f"the maximum number in this list is {max(list1)}")
+print(list1.count(5))
