91a89a2e0cb5e0c03f82b22c1948f5b7f58331a3
Arjun Chandrasekaran <c14.arjun@gmail.com>
2026-07-21 16:47:59 +0530

    commiting this one final time

---
+++ class203.py
@@ -0,0 +1,7 @@
+Country_code={'India':'0091',
+             'Australia': '0025',
+             'Nepal': '00977'}
+print("Country code for India- ")
+print(Country_code.get('India','Not found'))
+print("Country code for Japan- ")
+print(Country_code.get('Japan','Not found'))
